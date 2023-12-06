fcc889075d6e726beb64a895db8eaf1cb0f26fda
SHeraxanov Muham(shoxrux) <152672271+muhammaddevv@users.noreply.github.com>
2023-12-06 07:42:31 +0500

    yangi  .get metodi qo'shildi

--- 14-dictionary/kodlar-14-dars.py
+++ 14-dictionary/kodlar-14-dars.py
@@ -14,33 +14,29 @@ print(car_0["model"])
 print(car_0["rang"])
 
 # # Lug'atda istalgan ma'lumot turlarini saqlash mumkin
-# talaba_0 = {'ism':'murod olimov','yosh':20,'t_yil':2000}
-# print(f"{talaba_0['ism'].title()},\
-#  {talaba_0['t_yil']}-yilda tu'gilgan,\
-#  {talaba_0['yosh']} yoshda")
 
-# # Yangi kalit so'z va qiymat qo'shish
-# talaba_0['kurs'] = 4
-# talaba_0['fakultet'] = 'informatika'
-# talaba_0['ism'] = 'abdulloh'
+# Yangi kalit so'z va qiymat qo'shish
+talaba_0['kurs'] = 4
+talaba_0['fakultet'] = 'informatika'
+talaba_0['ism'] = 'abdulloh'
 
-# # Bo'sh lug'at
-# talaba_1 = {}
-# talaba_1['ism'] = 'qobil rasulov'
-# talaba_1['kurs'] = 3
-# talaba_1['yosh'] = 20
-# print(talaba_1)
-# print(f"Talaba {talaba_1['ism'].title()} {talaba_1['kurs']}-kurs")
+# Bo'sh lug'at
+talaba_1 = {}
+talaba_1['ism'] = 'qobil rasulov'
+talaba_1['kurs'] = 3
+talaba_1['yosh'] = 20
+print(talaba_1)
+print(f"Talaba {talaba_1['ism'].title()} {talaba_1['kurs']}-kurs")
 
 # # Qiymatni yangilash
-# talaba_1['kurs'] = 4
-# print(f"Talaba {talaba_1['ism'].title()} {talaba_1['kurs']}-kurs")
+talaba_1['kurs'] = 4
+print(f"Talaba {talaba_1['ism'].title()} {talaba_1['kurs']}-kurs")
 
-# # Kalit so'z-qiymat ni o'chirib tashlashj
-# talaba_0 = {'ism':'murod olimov','yosh':20,'t_yil':2000}
-# print(talaba_0)
-# del talaba_0['yosh']
-# print(talaba_0)
+# Kalit so'z-qiymat ni o'chirib tashlashj
+talaba_0 = {'ism':'murod olimov','yosh':20,'t_yil':2000}
+print(talaba_0)
+del talaba_0['yosh']
+print(talaba_0)
 
 # # Lu'gatlarni bir nechta qatorda yozish
 # telefonlar = {
